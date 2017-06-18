558ceca65669c48e834b67e872ecfb17f9e58419
Anna Winkler <me.anna.winkler@gmail.com>
2017-06-18 13:07:52 -0600

    Add more examples

--- cocktaildb-tests.sh
+++ cocktaildb-tests.sh
@@ -2,14 +2,16 @@
 
 echo ""
 
-COCKTAIL_DB_API=http://www.thecocktaildb.com/api/json/v1/1/
+# This uses the test API key 1. Don't abuse this!
+COCKTAIL_DB_API=http://www.thecocktaildb.com/api/json/v1/1
+
 if ! type "jq" > /dev/null; then
   echo "You need to install 'jq' to run these tests. You can get it here: http://stedolan.github.io/jq/"
   exit 1
 fi
 
 echo "Here is a simple way to format a JSON response:"
-curl -s /dev/null http://www.thecocktaildb.com/api/json/v1/1/random.php | jq .
+curl -s /dev/null ${COCKTAIL_DB_API}/random.php | jq .
 
 echo -n "GET a random recipe"
 STATUS=$(curl -so /dev/null -w '%{http_code}' ${COCKTAIL_DB_API}/random.php)
@@ -28,3 +30,11 @@ else
     echo -n -e "... \033[31mFailed"
 fi
 echo -e "\033[0m"
+
+echo -n "Count the number of Vodka drinks... "
+COUNT=$(curl -s ${COCKTAIL_DB_API}/filter.php\?i\=Vodka | jq '.drinks | length')
+echo "There are ${COUNT} Vodka drinks."
+
+echo -n "How many drinks don't have thumbnail images? ..."
+NULL_THUMBNAIL_COUNT=$(curl -s ${COCKTAIL_DB_API}/filter.php\?i\=Vodka | jq '[.drinks[] | select(.strDrinkThumb == null)] | length')
+echo " ${NULL_THUMBNAIL_COUNT}"
